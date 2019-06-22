f3d97a84598ee53f7f5b241f01a726526f5d1947
kuri <andybagadiong@gmail.com>
2019-06-22 19:18:15 +0800

    Created Server and Default Route

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const mysql = require('mysql')
+const express = require('express')
+const app = express()
 
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     database: 'inventory'
+})
+
+app.listen(3000,()=>{
+    console.log('Server is up and running..')
+})
+app.get("/",(req,res)=>{
+    console.log("Hi Server")
+    res.send("Hello from Andy")
 })
