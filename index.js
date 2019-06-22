d1d10ad5789a2623fd1b00f945614fcb8f6f6fd9
kuri <andybagadiong@gmail.com>
2019-06-22 19:23:22 +0800

    Added route for getting all data in items table

--- index.js
+++ index.js
@@ -11,7 +11,24 @@ const connection = mysql.createConnection({
 app.listen(3000,()=>{
     console.log('Server is up and running..')
 })
+//DEFAULT ROUTE
 app.get("/",(req,res)=>{
     console.log("Hi Server")
     res.send("Hello from Andy")
+})
+
+//GET All Item
+app.get("/inventory/items/",(req,res)=>{
+    console.log("Getting all data...")
+
+    const query = "SELECT * FROM items"
+    connection.query(query,(err,rows,fields)=>{
+        if (err){
+            console.log("Failed to query the items: " + err)
+            res.sendStatus(500)
+            return
+        }
+    res.json(rows)
+    })
+     
 })
